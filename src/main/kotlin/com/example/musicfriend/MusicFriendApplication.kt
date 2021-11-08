864424542ba4fbded011744ab3273071e579e4c6
ginny <sh.jeong@anypointmedia.com>
2021-11-08 22:20:57 +0900

    MUS-1 프로젝트 구성
    - 프로젝트 시작

---
+++ src/main/kotlin/com/example/musicfriend/MusicFriendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.musicfriend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MusicFriendApplication
+
+fun main(args: Array<String>) {
+    runApplication<MusicFriendApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/musicfriend/MusicFriendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.musicfriend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MusicFriendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
